88a7156f139babbd6527f2c2101a39aaa25367b5
Ameer Hamza <hamzaataariq12@gmail.com>
2024-11-12 10:10:35 +0500

    committed

--- src/Table.tsx
+++ src/Table.tsx
@@ -10,11 +10,11 @@ function Table() {
         {taskList.map((item, index) => (
           <tbody key={index}>
             <tr className={`${item.checked ? 'bg-red-300' : 'bg-blue-200'} flex justify-center items-center`}>
-              <td className="p-2 min-w-56 md:min-w-96 md:max-w-96 ">
+              <td className="p-2 min-w-64 md:min-w-96 md:max-w-96 ">
                 <input type="checkbox" onClick={() => toggleTaskChecked(index)} className={`mx-2 my-4`} checked={item.checked}/>
                 <span className={`${item.checked ? 'line-through' : ''}`}>{item.task}</span>
               </td>
-              <td className="px-3 py-2 flex md:flex-row mt-4">
+              <td className="px-3 py-2 flex md:flex-row">
                 <i className="fa-solid fa-pen-to-square mx-3 text-green-600 cursor-pointer"
                   onClick={() => {
                     const newTask = prompt("Edit Task:", item.task);
